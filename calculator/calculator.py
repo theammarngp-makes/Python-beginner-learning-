c06e96da6eea2787bf019ab25fa7a71952d64093
Mohammad Ammar <theammarngp@gmail.com>
2025-08-12 13:12:57 +0530

    Create Calculator/calculator.py

---
+++ calculator/calculator.py
@@ -0,0 +1,19 @@
+#Simple calculator
+#Input from the user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operetions = input("Choose any operetions (/,*,+,-): ")
+
+#adding conditionals 
+
+if operetions == '+':
+print("Result:", a + b)
+
+elif operetions == '-':
+print ('Result:', a-b)
+
+elif operetions == '':
+print ('Result:', ab)
+
+elif operetions == '/':
+print ('Result:' , a/b )
