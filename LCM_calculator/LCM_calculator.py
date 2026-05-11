57b52511c30c6576bbcc8fde164272318b0dd12c
Mohammad Ammar <theammarngp@gmail.com>
2026-05-11 22:40:03 +0530

    Improve LCM calculator code structure and output
    Refactored code for clarity and fixed input prompts.

--- LCM_calculator/LCM_calculator.py
+++ LCM_calculator/LCM_calculator.py
@@ -1,16 +1,15 @@
-#Program to find the Least Common Multiple (LCM) of two numbers
-#First taking user inpupt of two numbers
+# Program to find the Least Common Multiple (LCM) of two numbers
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
 
-a = int(input('Enter first number:'))
-b = int(input('Enter second number:'))
+# Start with the higher of the two numbers
+max_val = max(a, b)
 
-max_val = max(a,b)
+while True:
+    # Check if max_val is divisible by both a and b
+    if max_val % a == 0 and max_val % b == 0:
+        lcm = max_val
+        break
+    max_val += 1
 
-#creating a loop until both a and b are divisible
-
-while True :
-if max_val % a == 0 and max_val % b ==0 :
-lcm = max_val
-break
-max_val +=1
-print('LCM OF' ,a,'and',b, 'is', lcm)
+print(f'LCM of {a} and {b} is {lcm}')
